709147659edd4169aa25c6aac52ae8730b8e17e3
galaxyzflip <pigcs11@gmail.com>
2022-11-03 17:56:57 +0900

    18

--- ex04/src/main/java/org/zerock/aop/LogAdvice.java
+++ ex04/src/main/java/org/zerock/aop/LogAdvice.java
@@ -11,7 +11,7 @@ import lombok.extern.log4j.Log4j;
 @Component
 public class LogAdvice {
 	
-	@Before("execution(*org.zerock.service.SampleSerice*.*(..))")
+	@Before("execution(* org.zerock.service.SampleService*.*(..))")
 	public void logBefore() {
 		log.info("==============================");
 	}
